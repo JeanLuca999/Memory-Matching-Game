5ef2b2086dcdcb5e86b4f5214e666f6fde9ec9de
JeanLuca999 <66375859+JeanLuca999@users.noreply.github.com>
2021-10-01 09:36:13 -0300

    estrutura inicial

---
+++ src/components/Header/index.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__h1">Memory</h1>
+      <h2 className="header__h2">Matching Game</h2>
+    </header>
+  )
+}
